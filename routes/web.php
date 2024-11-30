634bed5e513fa890986404ba9df88f6cfcd4efef
pola5h <kzaman3055@gmail.com>
2024-11-30 12:35:40 +0600

    feat(routes): redirect root route to login page

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\UserDataController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
